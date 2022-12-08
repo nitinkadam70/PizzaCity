a84b5c26fa010a320598015a71c1a166a536dc45
Nitin Kadam <99539536+nitinkadam70@users.noreply.github.com>
2022-12-08 23:31:43 +0530

    upadate

--- client/pizzacity/src/pages/Offers.jsx
+++ client/pizzacity/src/pages/Offers.jsx
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
 
 const Offers = () => {
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080')
+      .then((res) => console.log('Hello', res.data.pizzaCity));
+  }, []);
+
   return <div>Offers</div>;
 };
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const { urlencoded } = require('express');
 const express = require('express'); //Library
 const fs = require('fs');
 const { parse } = require('querystring');
@@ -9,15 +8,13 @@ const app = express(); //Server
 app.use(express.json()); //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Homepage Pizzacity');
-});
+
 //Read Data or Get Data
-app.get('/dishes', (req, res) => {
+app.get('/', (req, res) => {
   fs.readFile('./db.json', { encoding: 'utf-8' }, (err, data) => {
-    //res.setHeader('Content-type', 'application/json');
+    res.setHeader('Content-type', 'application/json');
     //if we wanna send json data then just res.json(JSON.parse(data))
-    res.json(JSON.parse(data));
+    res.send(data);
   });
 });
 
