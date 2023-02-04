7330c35c8d74e054a36ce09304525ee4b92273b2
Nitin Kadam <99539536+nitinkadam70@users.noreply.github.com>
2023-02-04 20:13:54 +0530

    Optimizing performance and applying lazy loading

--- client/pizzacity/src/App.js
+++ client/pizzacity/src/App.js
@@ -1,16 +1,21 @@
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AboutUs from './pages/AboutUs';
-import Footer from './components/Footer';
-import Menu from './pages/Menu';
-import Offers from './pages/Offers';
+import Navbar from './components/Navbar';
+// import Homepage from './pages/Homepage';
+// import AboutUs from './pages/AboutUs';
+// import Menu from './pages/Menu';
+// import Offers from './pages/Offers';
+// import Footer from './components/Footer';
 import Error from './components/Error';
 import Reviews from './pages/Reviews';
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
 import GoToTop from './components/GoToTop';
-import AdminAuth from './pages/AdminAuth';
+const LazyHomepage = lazy(() => import('./pages/Homepage'));
+const LazyAbout = lazy(() => import('./pages/AboutUs'));
+const LazyMenu = lazy(() => import('./pages/Menu'));
+const LazyOffers = lazy(() => import('./pages/Offers'));
+const LazyFooter = lazy(() => import('./components/Footer'));
 
 //adding google Analytics to project
 const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
@@ -25,16 +30,80 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/offers" element={<Offers />} />
+        <Route
+          path="/"
+          element={
+            <Suspense
+              fallback={
+                <div
+                  className="spinner-border text-light"
+                  role="status"
+                ></div>
+              }
+            >
+              <LazyHomepage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/aboutus"
+          element={
+            <Suspense
+              fallback={
+                <div
+                  className="spinner-border text-light"
+                  role="status"
+                ></div>
+              }
+            >
+              <LazyAbout />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <Suspense
+              fallback={
+                <div
+                  className="spinner-border text-light"
+                  role="status"
+                ></div>
+              }
+            >
+              <LazyMenu />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/offers"
+          element={
+            <Suspense
+              fallback={
+                <div
+                  className="spinner-border text-light"
+                  role="status"
+                ></div>
+              }
+            >
+              <LazyOffers />
+            </Suspense>
+          }
+        />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/adminauth" element={<AdminAuth />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <GoToTop />
-      <Footer />
+      <Suspense
+        fallback={
+          <div
+            className="spinner-border text-light"
+            role="status"
+          ></div>
+        }
+      >
+        <LazyFooter />
+      </Suspense>
     </>
   );
 }
